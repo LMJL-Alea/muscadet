dfbea8642b711c6130f5c50ec19dbc28e02fb63f
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-11-23 16:05:19 +0100

    Update fitting unit tests to use same data across OSes.

--- tests/testthat/test-mle.R
+++ tests/testthat/test-mle.R
@@ -1,12 +1,4 @@
 test_that("fit_via_mle() works", {
-  set.seed(1234)
-  df <- rbidpp(
-    n = 1,
-    rho1 = 100, rho2 = 100,
-    alpha1 = 0.03, alpha2 = 0.03,
-    alpha12 = 0.035, tau = 0.5,
-    L = 1, model = "Gauss"
-  )
-  res <- fit_via_mle(df[[1]])
+  res <- fit_via_mle(sim_gauss0[[1]])
   expect_snapshot(res)
 })
